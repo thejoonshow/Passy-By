91c85bc2b4b26cf8be49978578ea81dddacdbe04
thejoonshow <thejoonshow@gmail.com>
2014-11-01 02:26:53 -0400

    bar

--- gen/com/app/dev/dreamteam/passby/R.java
+++ gen/com/app/dev/dreamteam/passby/R.java
@@ -1077,10 +1077,12 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
-        public static final int layer_card_background=0x7f020058;
+        public static final int ic_action_overflow=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
+        public static final int layer_card_background=0x7f020059;
     }
     public static final class id {
+        public static final int a_More=0x7f05003e;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1092,7 +1094,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050040;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1108,14 +1110,15 @@ containing a value of this type.
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
-        public static final int home_item=0x7f05003c;
+        public static final int home_item=0x7f05003f;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int info_item=0x7f05003f;
+        public static final int info_item=0x7f050042;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int login_item=0x7f05003e;
+        public static final int listview=0x7f05003d;
+        public static final int login_item=0x7f050041;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1133,6 +1136,7 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
+        public static final int secondLine=0x7f05003c;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
@@ -1144,7 +1148,7 @@ containing a value of this type.
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
-        public static final int user_item=0x7f05003d;
+        public static final int user_item=0x7f050040;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1199,7 +1203,8 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_pass=0x7f030018;
         public static final int activity_user=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int pass_listview=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int pass=0x7f0c0000;
--- src/com/app/dev/dreamteam/passby/PassUserActivity.java
+++ src/com/app/dev/dreamteam/passby/PassUserActivity.java
@@ -3,11 +3,12 @@ package com.app.dev.dreamteam.passby;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class PassUserActivity extends Activity{
-	
+public class PassUserActivity extends Activity {
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_user);
 	}
+
 }
